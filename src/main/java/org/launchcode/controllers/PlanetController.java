49a31b62f8c85408adff8d07d90628ca632db10e
alexdavies440 <alexdavies440@gmail.com>
2024-10-19 20:52:32 -0500

    Created Planet enum, started on PlanetController

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,22 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        // The compiler automatically adds some special methods when it creates an enum.
+        // For example, they have a static values method that returns an array containing
+        // all of the values of the enum in the order they are declared.
+        // This method is commonly used in combination with the for-each construct
+        // to iterate over the values of an enum type.
+        // Here you are naming that array "planets"
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
---
+++ src/main/java/org/launchcode/data/Planets.java
@@ -0,0 +1,5 @@
+package org.launchcode.data;
+
+public enum Planets {
+
+}
